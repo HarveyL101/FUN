f3a14ba0a5ee96fb46d2e3d4b2abafb66664a3fa
Harvey Lopez <up2118183@myport.ac.uk>
2025-02-27 19:08:33 +0000

    Added: Week5.hs Completed.

--- Week5.hs
+++ Week5.hs
@@ -3,7 +3,6 @@
 -}
 
 import Prelude hiding (fst, snd, head, tail, sum, concat, reverse, zip)
-import System.Win32 (xBUTTON1)
 
 -- Definitions of the prelude functions fst and snd
 
@@ -85,4 +84,93 @@ listLength (_ : xs) = 1 + listLength xs
 -- 5. Write a recursive function multAll that returns the product of all integers in a list.
 multAll :: [Int] -> Int
 multAll [] = 1
-multAll (x : xs) = x * (multAll xs)
+multAll (x : xs) = x * multAll xs
+
+-- 6. Use the && operator to write a recursive function andAll which returns the 
+-- conjunction (and) of all the elements of a list. This is a reimplementation of 
+-- the Prelude’s and function so do not use this function in your solution.
+andAll :: [Bool] -> Bool
+andAll [] = True
+andAll [x] = x
+andAll (x : xs) = x && andAll xs
+
+-- 7. Write a recursive orAll function that returns the disjunction of all elements
+-- in the given list (uses the || operator). Hint: Start by thinking about how the 
+-- following expression should be evaluated: orAll[]
+
+orAll :: [Bool] -> Bool
+orAll [] = False
+orAll[x] = x
+orAll (x:xs) = x || orAll xs
+
+-- 8. Write a function countIntegers that counts how many times a given integer
+-- appears in a list of integers. This function will be similar in structure to worked example 1.
+countIntegers :: Int -> [Int] -> Int
+countIntegers _ [] = 0
+countIntegers x (y : ys)
+    | x == y    = 1 + countIntegers x ys
+    | otherwise = countIntegers x ys
+
+-- 9. Write a recursive function removeAll that removes all instances of 
+-- a given number from a list of integers.
+removeAll :: Int -> [Int] -> [Int]
+removeAll _ [] = []
+removeAll x (y:ys)
+    | x == y    = removeAll x ys
+    | otherwise = y : removeAll x ys
+
+-- 10. Using removeAll, write a removeAllButFirst function.
+removeAllButFirst :: Int -> [Int] -> [Int]
+removeAllButFirst _ [] = []
+removeAllButFirst x (y:ys)
+    | x == y    = y : removeAll x ys
+    | otherwise = y : removeAllButFirst x ys
+
+-- 11. Recall the StudentMark type synonym from last week. 
+-- Write a function that gives a list of the marks for a particular student.
+-- Added for unit testing purposes
+type StudentMark = (String, Int)
+testData :: [StudentMark]
+testData = [
+    ("John", 53),
+    ("Sam", 16),
+    ("Kate", 85),
+    ("Jill", 65),
+    ("Bill", 37),
+    ("Amy", 22),
+    ("Jack", 41),
+    ("Sue", 71)
+    ]
+
+listMarks :: String -> [StudentMark] -> [Int]
+listMarks _ [] = []
+listMarks name ((student, mark):xs)
+    | name == student = mark : listMarks name xs
+    | otherwise       = listMarks name xs
+
+-- 12. Write a recursive function sorted which decides if the elements of a 
+-- list are sorted (the output needs to be of type Bool).
+sorted :: [Int] -> Bool
+sorted [] = True
+sorted [_] = True
+sorted (x:y:ys)
+    | x < y = sorted ys
+    | otherwise = False
+
+-- 13. Write a recursive function prefix which decides if the first list 
+-- of integers is a prefix of the second list.
+prefix :: [Int] -> [Int] -> Bool
+prefix [] _ = True
+prefix _ [] = False
+prefix (x : xs) (y : ys)
+    | x == y    = prefix xs ys
+    | otherwise = False
+
+-- 14. [Harder] Using your prefix function, write a recursive function 
+-- subSequence that decides if the first list is contained in the second.
+subSequence :: [Int] -> [Int] -> Bool
+subSequence [] _ = True
+subSequence _ [] = False
+subSequence xs ys
+    | prefix xs ys = True
+    | otherwise    = subSequence xs (tail ys)
