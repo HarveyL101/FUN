19647170d3f9bb41c7bf3c5c00e09179ded3c516
Harvey Lopez <up2118183@myport.ac.uk>
2025-04-01 02:30:51 +0100

    Added: Week 7 completed.

--- Week7.hs
+++ Week7.hs
@@ -23,8 +23,8 @@ betterStudent (Student s1 m1) (Student s2 m2)
     | otherwise         = s2
 
 -- Shapes algebraic type 
-data Shape = Circle Float |
-             Rectangle Float Float
+data Shape = Circle Float | Rectangle Float Float
+  deriving (Show)
 
 area :: Shape -> Float
 area (Circle r)      = pi * r * r
@@ -49,8 +49,7 @@ testTree = Node 20 (Node 3 (Node 12 Null Null) (Node 7 Null Null))
                   (Node 8 (Node 4 (Node 6 Null Null) Null) Null)
 
 -- Binary search tree test data
-testSearchTree =  Node 5 (Node 1 Null Null)
-                         (Node 8 (Node 7 Null Null) Null)
+testSearchTree =  Node 5 (Node 1 Null Null) (Node 8 (Node 7 Null Null) Null)
 
 height :: Tree -> Int
 height Null = 0
@@ -150,10 +149,142 @@ formatCar cars i = printf "%d- %s %s costs %.2f pounds" (i + 1) (getMake c) (get
   1. Define an algebraic type Month that represents the twelve months of the year and
   Season that represents the four seasons. See the test cases for exercise 2.
 -}
-
-
+data Month = January | February | March | April | May | June | July | August | September | October | November | December
+  deriving (Eq, Ord, Show)
+data Season = Spring | Summer | Autumn | Winter
+  deriving (Show)
 {-
   2. Define a function season that maps months onto (meteorological) seasons. See the
   test cases showing the outputs for February and March. (All seasons are all three
   months long).
 -}
+season :: Month -> Season
+season m 
+  | m `elem` [December, January, February]  = Winter
+  | m `elem` [March, April, May]            = Spring
+  | m `elem` [June, July, August]           = Summer
+  | m `elem` [September, October, November] = Autumn
+
+{-
+  3. Define a function numberOfdays that takes a month and a year and returns the number
+  of days in that month. Assume all years divisible by four are leap years.
+-}
+isLeapYear :: Int -> Bool
+isLeapYear year = (year `mod` 4 == 0 && year `mod` 100 /= 0) || (year `mod` 400 == 0) 
+
+numberOfDays :: Month -> Int -> Int
+numberOfDays February year
+  | isLeapYear year = 29
+  | otherwise       = 28
+numberOfDays month year
+  | month `elem` [April, June, September, November] = 30
+  | otherwise                                       = 31
+
+{-
+  Points And Shapes:
+
+  4. Define an algebraic type Point which should represent the coordinates of points in
+  two-dimensional space.
+-}
+data Point = Point Int Int
+  deriving (Show)
+
+{-
+  5. Using Shape and Point, define a data type called PositionedShape which combines a
+  shape and with its centre point
+-}
+data PositionedShape = PositionedShape Shape Point
+  deriving (Show)
+
+{-
+  6. Define a function move with the signature below that moves a given PositionedShape
+  by the given x and y distances.
+  ```
+  move :: PositionedShape -> Float -> Float -> PositionedShape
+  ```
+  Note that you will need to add deriving (Show) to the data types from exercises 4
+  and 5.
+-}
+move :: PositionedShape -> Float -> Float -> PositionedShape
+move (PositionedShape shape (Point px py)) x y = PositionedShape shape (Point (px + round x) (py + round y))
+
+{-
+  Functions for the binary tree type
+
+  7. Week7.hs defines a recursive type Tree for a binary tree and a testTree value of this
+  type. Define a function numberOfNodes that returns the number of nodes in a given
+  tree.
+-}
+numberOfNodes :: Tree -> Int
+numberOfNodes Null = 0
+numberOfNodes (Node _ left right) = 1 + numberOfNodes left + numberOfNodes right
+
+{-
+  8. Define a function with the signature below that determines if a value exists in a tree.
+  ```
+  isMember :: Int -> Tree -> Bool
+  ```
+-}
+isMember :: Int -> Tree -> Bool
+isMember _ Null = False
+isMember x (Node root left right)
+  | root == x = True
+  | otherwise = isMember x left || isMember x right
+
+{-
+  9. Define a function leaves with the signature below that returns the list of all the leaves
+  of the tree. Leaves are nodes that have Null as both subtrees (see the test case).
+  ```
+  leaves :: Tree -> [Int]
+  ```
+-}
+leaves :: Tree -> [Int]
+leaves Null = []
+leaves (Node root Null Null) = [root]
+leaves (Node _ left right) = leaves left ++ leaves right
+
+{-
+  10. Define a function inOrder that lists the elements of a tree according to the in-order
+  traversal.
+
+  If the tree is a valid binary search tree, this function will give a list of the tree’s
+  elements in ascending numerical order. Try it on the testBinaryTree below.
+-}
+testBinaryTree = Node 5 (Node 1 Null Null) (Node 8 (Node 7 Null Null) Null)
+
+inOrder :: Tree -> [Int]
+inOrder Null = []
+inOrder (Node root left right) = inOrder left ++ [root] ++ inOrder right
+
+{-
+  11. Define a function insert which inserts a new value into a tree. The function should
+  assume that the tree is a binary search tree and should preserve this property.
+
+  insert :: Int -> Tree -> Tree
+  
+  Test your function by inserting 2 to testBinaryTree defined in exercise 10. It should
+  produce the following tree:
+  
+  Node 5 (Node 1 Null (Node 2 Null Null)) (Node 8 (Node 7 Null Null) Null)
+  
+  If you struggle with the concept, try this visualisation tool in your browser and insert
+  the following values one after another: 5, 1, 7, 8, 2.
+  https://www.cs.usfca.edu/~galles/visualization/BST.html
+-}
+
+insert :: Int -> Tree -> Tree
+insert x Null = Node x Null Null
+insert x (Node root left right)
+  | x < root  = Node root (insert x left) right
+  | x > root  = Node root left (insert x right)
+  | otherwise = Node root left right
+
+{-
+  12. Define a function listToSearchTree which creates a binary search tree from a list of
+  integers. You need to insert elements in the order that they appear in the list.
+
+  Finally, using listToSearchTree and inOrder, write another function binaryTreeSort
+  that sorts a lit of integers.
+-}
+listToSearchTree :: [Int] -> Tree
+listToSearchTree arr = foldl (\tree x -> insert x tree) Null arr
